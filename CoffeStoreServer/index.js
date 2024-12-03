2fb84cf4f9c02992d9750db775078aeae89206f8
Shahriar Ahmed <shahriarabiddut@gmail.com>
2024-12-04 01:14:04 +0600

    Vercel Issue Solved!

--- CoffeStoreServer/index.js
+++ CoffeStoreServer/index.js
@@ -23,8 +23,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
     // DB Collection
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
     const userCollection = client.db("coffeeDB").collection("users");
@@ -124,10 +122,6 @@ async function run() {
     console.log('Delete Coffee',query);
     res.send(result);
   })
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
